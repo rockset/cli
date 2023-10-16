0d354ede1747b79bbf2304fb1d092fd2c04bb5cf
Martin Englund <pme@rockset.com>
2023-10-16 07:42:39 -0700

    fix broken test

--- config/config_test.go
+++ config/config_test.go
@@ -39,7 +39,7 @@ func TestStore_noFile(t *testing.T) {
 	require.NoError(t, err)
 
 	f, err := os.ReadFile(file)
-	assert.Equal(t, "", string(f))
+	assert.Equal(t, `current: \"\"\nkeys: {}\ntokens: {}\n`, string(f))
 }
 
 func TestConfig(t *testing.T) {
